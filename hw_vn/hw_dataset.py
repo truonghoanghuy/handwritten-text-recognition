931883292522c675f636d789a03e78337fc0168c
Trương Hoàng Huy <hoanghuy1611@gmail.com>
2020-04-25 00:15:27 +0700

    Vietnamese HWR (SFR)

---
+++ hw_vn/hw_dataset.py
@@ -0,0 +1,109 @@
+import os
+import random
+
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from hw import grid_distortion
+from utils import string_utils, safe_load, augmentation
+
+PADDING_CONSTANT = 0
+
+
+def collate(batch):
+    batch = [b for b in batch if b is not None]
+    # These all should be the same size or error
+    assert len(set([b['line_img'].shape[0] for b in batch])) == 1
+    assert len(set([b['line_img'].shape[2] for b in batch])) == 1
+
+    dim0 = batch[0]['line_img'].shape[0]
+    dim1 = max([b['line_img'].shape[1] for b in batch])
+    dim2 = batch[0]['line_img'].shape[2]
+
+    all_labels = []
+    label_lengths = []
+
+    input_batch = np.full((len(batch), dim0, dim1, dim2), PADDING_CONSTANT).astype(np.float32)
+    for i in range(len(batch)):
+        b_img = batch[i]['line_img']
+        input_batch[i, :, :b_img.shape[1], :] = b_img
+
+        L = batch[i]['gt_label']
+        all_labels.append(L)
+        label_lengths.append(len(L))
+
+    all_labels = np.concatenate(all_labels)
+    label_lengths = np.array(label_lengths)
+
+    line_imgs = input_batch.transpose([0, 3, 1, 2])
+    line_imgs = torch.from_numpy(line_imgs)
+    labels = torch.from_numpy(all_labels.astype(np.int32))
+    label_lengths = torch.from_numpy(label_lengths.astype(np.int32))
+
+    return {
+        "line_imgs": line_imgs,
+        "labels": labels,
+        "label_lengths": label_lengths,
+        "gt": [b['gt'] for b in batch]
+    }
+
+
+class HwDataset(Dataset):
+    def __init__(self, set_list, char_to_idx, augmentation=False, img_height=32, random_subset_size=None):
+
+        self.img_height = img_height
+
+        self.ids = set_list
+        self.ids.sort()
+
+        if random_subset_size is not None:
+            self.ids = random.sample(self.ids, min(random_subset_size, len(self.ids)))
+
+        self.char_to_idx = char_to_idx
+        self.augmentation = augmentation
+        self.warning = False
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        img_path, gt_path = self.ids[idx]
+        gt = open(gt_path, encoding='utf8')
+        if gt is None:
+            return None
+
+        img = cv2.imread(img_path)
+
+        if img is None:
+            return None
+
+        if img.shape[0] != self.img_height:
+            if img.shape[0] < self.img_height and not self.warning:
+                self.warning = True
+                print("WARNING: upsampling image to fit size")
+            percent = float(self.img_height) / img.shape[0]
+            img = cv2.resize(img, (0, 0), fx=percent, fy=percent, interpolation=cv2.INTER_CUBIC)
+
+        if img is None:
+            return None
+
+        if self.augmentation:
+            img = augmentation.apply_random_color_rotation(img)
+            img = augmentation.apply_tensmeyer_brightness(img)
+            img = grid_distortion.warp_image(img)
+
+        img = img.astype(np.float32)
+        img = img / 128.0 - 1.0
+
+        gt = gt.read()
+        if len(gt) == 0:
+            return None
+        gt_label = string_utils.str2label_single(gt, self.char_to_idx)
+
+        return {
+            "line_img": img,
+            "gt": gt,
+            "gt_label": gt_label
+        }
---
+++ hw_vn_training.py
@@ -0,0 +1,62 @@
+import json
+import os
+import sys
+
+import torch
+import yaml
+from torch import nn
+from torch.utils.data import DataLoader
+
+from hw_vn import cnn_lstm
+from hw_vn import hw_dataset
+from hw_vn import hw_loss_function
+from hw_vn.hw_dataset import HwDataset
+from utils import module_trainer
+from utils.dataset_parse import load_file_list
+from utils.dataset_wrapper import DatasetWrapper
+
+# noinspection DuplicatedCode
+if __name__ == '__main__':
+    with open(sys.argv[1]) as f:
+        config = yaml.load(f)
+    hw_network_config = config['network']['hw']
+    char_set_path = hw_network_config['char_set_path']
+    with open(char_set_path, encoding='utf8') as f:
+        char_set = json.load(f)
+    idx_to_char = {int(k): v for k, v in char_set['idx_to_char'].items()}
+    train_config = config['training']
+    batches_per_epoch = int(train_config['hw']['images_per_epoch'] / train_config['hw']['batch_size'])
+    checkpoint_filepath = os.path.join(train_config['snapshot']['best_validation'], 'hw_vn_checkpoint.pt')
+
+    train_set_list = load_file_list(train_config['training_set'])
+    train_dataset = HwDataset(train_set_list, char_set['char_to_idx'], augmentation=True,
+                              img_height=hw_network_config['input_height'])
+    train_dataloader = DataLoader(train_dataset, batch_size=train_config['hw']['batch_size'], shuffle=False,
+                                  num_workers=0, collate_fn=hw_dataset.collate)
+    train_dataloader = DatasetWrapper(train_dataloader, batches_per_epoch)
+
+    eval_set_list = load_file_list(train_config['validation_set'])
+    eval_dataset = HwDataset(eval_set_list, char_set['char_to_idx'], img_height=hw_network_config['input_height'],
+                             random_subset_size=train_config['hw']['validation_subset_size'])
+    eval_dataloader = DataLoader(eval_dataset, batch_size=train_config['hw']['batch_size'], shuffle=False,
+                                 num_workers=0, collate_fn=hw_dataset.collate)
+
+    dtype = torch.float32
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    hw = cnn_lstm.create_model(hw_network_config).to(device)
+    optimizer = torch.optim.Adam(hw.parameters(), lr=train_config['hw']['learning_rate'])
+    criterion = nn.CTCLoss(reduction='sum', zero_infinity=True)
+
+
+    def calculate_hw_train_loss(hw_model, input):
+        return hw_loss_function.calculate_hw_loss(hw_model, input, dtype, device, criterion, idx_to_char, train=True)
+
+
+    def calculate_hw_evaluate_loss(hw_model, input):
+        return hw_loss_function.calculate_hw_loss(hw_model, input, dtype, device, criterion, idx_to_char, train=False)
+
+
+    trainer = module_trainer.ModuleTrainer(hw, optimizer, calculate_hw_train_loss, calculate_hw_evaluate_loss,
+                                           train_dataloader, eval_dataloader, checkpoint_filepath)
+    resume = 'resume' in sys.argv
+    trainer.train(resume=resume, continuous_training=True)
